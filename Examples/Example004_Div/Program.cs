ae7e738f6a25af3d58a12eeb5bd3f37cc6aeb2ab
Andrey-Rya <dinamit2007@mail.ru>
2022-08-05 19:48:05 +0700

    add Example 004 Div

---
+++ Examples/Example004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿// Выполняем деление двух нецелых чисел
+double numberA = 12; // контейнер double работает с вещественными (нецелыми) числами
+double numberB = 5;
+Console.WriteLine(numberA / numberB);
