21e7e3fbbbcc6df8911e2fc47898493bca2a786a
Andrey-Rya <dinamit2007@mail.ru>
2022-08-05 19:18:15 +0700

    add Example 003 Sum

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа сложения 2-х целых чисел
+int numberA = 3; // определим переменную (int-целые числа) и положим в нее чило 3
+// скажем компьютеру сколько памяти нужно под это число выделить
+int numberB = 5;
+Console.WriteLine(numberA + numberB);
+// Второй вариант сложения 2-х целых чисел
+int numberAA = 31;
+int numberBB = 5;
+int result = numberAA + numberBB;
+Console.WriteLine(result);
