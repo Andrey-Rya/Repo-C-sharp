7b13740872dcceddf55898ecfe1dece4dc0eee87
Andrey-Rya <dinamit2007@mail.ru>
2022-08-06 00:24:49 +0700

    add Example005 Condition

---
+++ Examples/Example005_Condition/Program.cs
@@ -0,0 +1,14 @@
+﻿// Приветствуем Машу по особенному
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine(); // считываем данные с консоли в username
+
+if(username.ToLower() == "маша") // ToLover переводит все буквы ввода в нижн регистр
+{
+    Console.WriteLine("Ура, это же МАША!"); // приветствуем только Машу  
+}
+else
+{
+    Console.Write("Привет, "); // приветствуем всех остальных
+    Console.WriteLine(username); // приветствуем остальных (2 способ)  
+}
+
