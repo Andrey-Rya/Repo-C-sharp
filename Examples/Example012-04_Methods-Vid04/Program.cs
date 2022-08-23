3e44f4a2414237ce9f518b028ea57d2cb7828ab6
Andrey-Rya <dinamit2007@mail.ru>
2022-08-23 18:23:46 +0700

    Метод-04 (int группа) Что-то принимают и что-то  возвращают

---
+++ Examples/Example012-04_Methods-Vid04/Program.cs
@@ -0,0 +1,16 @@
+﻿// Метод-04 (int группа) Что-то принимают и что-то возвращают
+string Method4(int count, string text) // возвращаем строку с и друг за другом разместим count раз
+{
+    int i=0;
+    string result = String.Empty; // изначально это пустая строка String.Empty
+
+    while (i< count) // пока i<count делаем ниже
+    {
+        result = result + text; 
+        i++;
+    }
+    return result;
+}
+
+string res = Method4(10, "текст 10 раз, ");// вызываем метод и склеиваем текст 10 раз 
+Console.WriteLine(res);
