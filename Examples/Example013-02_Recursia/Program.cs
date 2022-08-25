a4d883b97dd05960eca1a5fb9bd27fd264eeddfc
Andrey-Rya <dinamit2007@mail.ru>
2022-08-25 22:51:13 +0700

    Урок 4-Вычисление факториала через рекурсию

---
+++ Examples/Example013-02_Recursia/Program.cs
@@ -0,0 +1,13 @@
+﻿// вычисление факториала через рекурсию
+double Factorial(int n) // double вместо int используем чтобы не было переполнения
+{
+	// 1! = 1
+	// 0! = 1
+	if (n == 1) return 1;
+	else return n * Factorial(n - 1);
+}
+for (int i = 1; i < 20; i++)
+{
+	Console.WriteLine($"{i}! = {Factorial(i)}");
+}
+
