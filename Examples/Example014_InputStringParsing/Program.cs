b583d16177a0b4bf393801897ecc1b99dd76f24a
Andrey-Rya <dinamit2007@mail.ru>
2022-09-01 09:15:24 +0700

    Урок 6-Написать парсинг входной строки. Увеличить в 2 раза каждую координату

---
+++ Examples/Example014_InputStringParsing/Program.cs
@@ -0,0 +1,16 @@
+﻿// Урок 6: написать парсинг входной строки. Увеличить в 2 раза 
+// каждую координату
+
+using System.Linq;
+                
+string text = "(1,2) (2,3) (4,5) (6,7)";
+
+var data = text.Split(" ");
+
+                .ToArray();
+
+for (int i = 0; i < data.Length; i++)
+{
+    Console.WriteLine(data[i]);
+}
+
